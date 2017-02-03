01f35a1b253d42496d7bb0c43a88f3c07432a81c
maximkuryazov <maximkuryazov@gmail.com>
2017-02-03 15:46:18 +0300

    User creation added

--- server/user.js
+++ server/user.js
@@ -30,34 +30,46 @@ module.exports = (function() {
 
 			var mongoose = private.mongoose;
 
-			var kittySchema = mongoose.Schema({
-				name: String
+			var userSchema = mongoose.Schema({
+				email: String,
+				password: String,
+				firstname: String,
+				lastname: String,
+				middlename: String,
+				age: Number,
+				birthday: Date,
+				sound: Boolean,
+				animations: Boolean,
+				visible: Boolean
 			});
 
-			kittySchema.methods.speak = function () {
-				var greeting = this.name ? "Meow name is " + this.name : "I don't have a name";
-				console.log(greeting);
+			userSchema.methods.showData = function () {
+				console.log("Email: " + this.email);
 			};
 
-			var Kitten = mongoose.model('Kitten', kittySchema);
-			var fluffy = new Kitten({ name: 'fluffy' });
+			var User = mongoose.model('User', userSchema);
 
-			fluffy.speak();
-
-			fluffy.save(function (err, fluffy) {
-				if (err) return console.error(err);
-				fluffy.speak();
+			var currentUser = new User({
+				email: "maximkuryazov@gmail.com",
+				password: "322538631",
+				age: 27,
+				birthday: new Date()
 			});
 
-			Kitten.find(function (err, kittens) {
-				if (err) return console.error(err);
-				console.log("Kittens: " + kittens);
-				for (var key in kittens) {
-					console.log("Name: " + kittens[key].name);
-					console.log("ID: " + kittens[key]._id);
+			currentUser.save(function (error, currentUser) {
+				if (error) return console.error(error);
+				currentUser.showData();
+			});
+
+			User.find(function (error, users) {
+				if (error) return console.error(error);
+				console.log("Users: " + users);
+				for (var key in users) {
+					console.log("Email: " + users[key].email);
+					console.log("ID: " + users[key]._id);
 				}
 			});
-			
+
 		}
 	};
 	return constructor;
