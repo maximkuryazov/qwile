f591a552d29bda9f4c97931d14353f97331bd964
maximkuryazov <maximkuryazov@gmail.com>
2017-02-04 15:03:41 +0300

    Mail sends after registration.

---
+++ server/mail.js
@@ -0,0 +1,33 @@
+/**
+ * Created by User on 2/4/2017.
+ */
+
+module.exports = (function() {
+
+	return {
+		send: function (options, successCallback, errorCallback) {
+
+			const nodeMailer = require('nodemailer');
+
+			var transporter = nodeMailer.createTransport({
+				service: 'Gmail',
+				auth: {
+					user: 'maximkuryazov@gmail.com',
+					pass: '322538631'
+				}
+			});
+
+			transporter.sendMail(options, function(error, info) {
+				if (error) {
+					console.log(error);
+					errorCallback(error);
+				} else {
+					console.log("Message sent: " + info.response);
+					successCallback(info);
+				}
+			});
+
+		}
+	}
+
+})();
